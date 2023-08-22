5a4439ec46c1286468a3a7a05091d62d0ccd62a1
aniket-git-hub <singhdharmvir81@gmail.com>
2023-08-22 16:38:33 +0530

    ADDED: new route for all the routes that are not matching | Error 404 page not found

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import Error404Page from './pages/Error404Page'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/login' element={<LoginPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='*' element={<Error404Page/>} />
       </Routes>
     </>
   )
