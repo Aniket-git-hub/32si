4f9c89960b85f5884539fbe3198103b2cec09d85
aniket-git-hub <singhdharmvir81@gmail.com>
2023-08-31 20:31:55 +0530

    ADDED: basic template for home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,31 +1,24 @@
 import { useAuth } from "../hooks/useAuth"
-import { Box, Heading, Text, Button, Icon } from "@chakra-ui/react"
+import { Box, Heading, Text, Button, Icon, Flex, Spacer, Avatar, HStack } from "@chakra-ui/react"
 import { StarIcon } from "@chakra-ui/icons"
 
 export default function HomePage() {
-  const { user, isAuthenticated, remove } = useAuth()
+  const { user, remove } = useAuth()
   const { username, name, email } = user
   const handleLogout = () => {
-    console.log(user, isAuthenticated)
     remove()
-    console.log(user, isAuthenticated)
-
   }
   return (
-    <Box p={4}>
-      <Text fontSize="xl" color="gray.600">
-        {isAuthenticated ? "You are logged in" : "You are not logged in"}
-      </Text>
-      <Box display="flex" alignItems="center" mt={4}>
-        <Icon as={StarIcon} w={8} h={8} mr={4} />
-        <Box>
-          <Heading as="h2" fontSize="2xl">{username}</Heading>
-          <Text fontSize="lg">{name}</Text>
-          <Text fontSize="lg">{email}</Text>
-        </Box>
-      </Box>
-      <Button colorScheme="red" mt={4} onClick={() => handleLogout()}>Logout</Button>
-
-    </Box>
+    <Flex as="nav" bg="gray.100" p=".5rem" minW="100vw" justifyContent="space-between" alignItems="center">
+      <Heading size="lg">32 Beads</Heading>
+      <Spacer></Spacer>
+      <HStack>
+        <Avatar size="sm" name={name} src='' />
+        <Text> sample@gamil.com</Text>
+        <Button size="sm" variant="outline" colorScheme="purple" onClick={handleLogout}>
+          logout
+        </Button>
+      </HStack>
+    </Flex>
   )
 }
