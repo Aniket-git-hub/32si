23d9ead44f8ce571bd331d29c8c65f2ed96114dc
aniket-git-hub <singhdharmvir81@gmail.com>
2023-09-01 09:23:34 +0530

    ADDED: routes for production and development

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -19,19 +19,25 @@ import resetPassword from "../controllers/auth/resetPassword.js"
  * Defining /register route 
  * Handles POST request to create new user or register as in the route
  */
-router.post("/register", uRIV, inputValidation, register)
+const registerRoute = process.env.NODE_ENV === 'production' ? process.env.REGISTER_USER_ROUTE : "/register" 
+router.post(registerRoute, uRIV, inputValidation, register)
 /**
  * Defining /login route 
  * Handles POST request to login the user 
  */
-router.post("/login", uLIV, inputValidation, login)
+const loginRoute = process.env.NODE_ENV === 'production' ? process.env.LOGIN_USER_ROUTE : "/login" 
+router.post(loginRoute, uLIV, inputValidation, login)
 
-router.post("/token/refresh", verifyJWT, refreshToken)
+const refreshTokenRoute = process.env.NODE_ENV === 'production' ? process.env.REFRESH_TOKEN_ROUTE : "/token/refresh" 
+router.post(refreshTokenRoute, verifyJWT, refreshToken)
 
-router.post("/forgot-password", fPEV, inputValidation, forgotPassword)
+const forgotPasswordRoute = process.env.NODE_ENV === 'production' ? process.env.FORGOT_PASSWORD_ROUTE : "/forgot-password" 
+router.post(forgotPasswordRoute, fPEV, inputValidation, forgotPassword)
 
-router.post("/forgot-password/verify-otp", fPOV, inputValidation, forgotPasswordVerifyOtp)
+const verifyOTPRoute = process.env.NODE_ENV === 'production' ? process.env.VERIFY_OTP_ROUTE : "/forgot-password/verify-otp" 
+router.post(verifyOTPRoute, fPOV, inputValidation, forgotPasswordVerifyOtp)
 
-router.post("/reset-password", rPV, inputValidation, resetPassword)
+const resetPasswordRoute = process.env.NODE_ENV === 'production' ? process.env.RESET_PASSWORD_ROUTE : "/reset-password" 
+router.post(resetPasswordRoute, rPV, inputValidation, resetPassword)
 
 export default router
