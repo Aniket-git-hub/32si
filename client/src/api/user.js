114e1b4c2ba25a30fc7b414f0048dc0bd8d9e962
aniket-git-hub <singhdharmvir81@gmail.com>
2023-09-09 20:57:15 +0530

    using getallusers api endpoint in rivals page

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -13,7 +13,7 @@ const handleRequest = async (endpoint, data, method) => {
         };
         switch (method) {
             case "GET":
-                response = await interceptorsInstance.get(endpoint, config);
+                response = await interceptorsInstance.get(`${endpoint}?page=${data?.page}&limit=${data?.limit}`, config);
                 break;
             case "POST":
                 response = await interceptorsInstance.post(endpoint, data, config);
@@ -35,7 +35,8 @@ const handleRequest = async (endpoint, data, method) => {
     }
 }
 
-const env = import.meta.env.VITE_ENV
 
-const updateUserEndpoint = env === 'production' ? import.meta.env.VITE_UPDATE_USER_ENDPOINT : '/user/'
-export const updateUser = async (data) => handleRequest(updateUserEndpoint, data, "PUT")
+const getEndpoint = (productionRoute, developmentRoute) => import.meta.env.VITE_ENV === 'production' ? import.meta.env[productionRoute] : developmentRoute
+
+export const updateUser = async (data) => handleRequest(getEndpoint('VITE_UPDATE_USER_ROUTE', '/user/'), data, "PUT")
+export const getAllUsers = async (data) => handleRequest(getEndpoint('VITE_ALL_USERS_ROUTE', '/user/users'), data, "GET")
--- client/src/pages/dashboard/Rivals.jsx
+++ client/src/pages/dashboard/Rivals.jsx
@@ -1,14 +1,26 @@
 import { Avatar, Button, Card, CardBody, CardHeader, Heading, Link, Text, Box, VStack, SimpleGrid, IconButton } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { MdVerified } from 'react-icons/md'
+import { getAllUsers } from '../../api/user'
 export default function Rivals() {
-    const list = [
+    const [list, setList] = useState([
         { name: "Aniket Singh", bio: "the quick brown fox jumped over the lazy dog." },
         { name: "Aniket Singh", bio: "the quick brown fox jumped over the lazy dog." },
         { name: "Aniket Singh", bio: "the quick brown fox jumped over the lazy dog." },
         { name: "Aniket Singh", bio: "the quick brown fox jumped over the lazy dog." },
         { name: "Aniket Singh", bio: "the quick brown fox jumped over the lazy dog." },
         { name: "Aniket Singh", bio: "the quick brown fox jumped over the lazy dog." },
-    ]
+    ])
+    useEffect(() => {
+        (async () => {
+                try {
+                    const response = await getAllUsers({ page: 1, limit: 10 })
+                    setList(response.data.users)
+                } catch (error) {
+                    console.log(error)
+                }
+            })()
+    }, [])
     return (
         <>
             <Heading size={"lg"}>Rivals</Heading>
