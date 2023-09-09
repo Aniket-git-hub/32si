fbd109867baeb4bd376cffbbeef66e7ecfafdeb1
aniket-git-hub <singhdharmvir81@gmail.com>
2023-09-09 20:24:16 +0530

    Updated: getAllusers controller to return users based location and friends list

--- server/controllers/user/getAllUser.js
+++ server/controllers/user/getAllUser.js
@@ -1,12 +1,73 @@
 import USER from "../../models/user.js"
+import mongoose from 'mongoose';
 
 async function getAllUser(req, res, next) {
     try {
         const page = parseInt(req.query.page) || 1
         const limit = parseInt(req.query.limit) || 10
         let skips = (page - 1) * 10
-        const users = await USER.find({}).skip(skips).limit(limit).exec()
-        const count = await USER.countDocuments({})
+
+        // Get the current user's friends
+        const currentUser = await USER.findById(req.user.id);
+        const friends = currentUser.friends;
+
+        let pipeline;
+
+        if (currentUser.location && currentUser.location.type === 'Point') {
+            pipeline = [
+                {
+                    $geoNear: {
+                        near: currentUser.location,
+                        distanceField: "dist.calculated",
+                        maxDistance: 100000,
+                        spherical: true
+                    }
+                },
+                { $match: { _id: { $nin: [...friends, currentUser._id] } } },
+                {
+                    $addFields: {
+                        friendsCount: { $size: "$friends" },
+                        gamesPlayedCount: { $size: "$gamesPlayed" }
+                    }
+                },
+                { $skip: skips },
+                { $limit: limit }
+            ];
+        } else {
+            pipeline = [
+                { $match: { _id: { $nin: [...friends, currentUser._id] } } },
+                {
+                    $addFields: {
+                        friendsCount: { $size: { $ifNull: ["$friends", []] } },
+                        gamesPlayedCount: { $size: { $ifNull: ["$gamesPlayed", []] } }
+                    }
+                },
+                { $sample: { size: limit } },
+                { $skip: skips },
+                { $limit: limit }
+            ];
+        }
+
+        let users = await USER.aggregate(pipeline);
+
+        if (users.length === 0 && currentUser.location && currentUser.location.type === 'Point') {
+            pipeline = [
+                { $match: { _id: { $nin: [...friends, currentUser._id] } } },
+                {
+                    $addFields: {
+                        friendsCount: { $size: { $ifNull: ["$friends", []] } },
+                        gamesPlayedCount: { $size: { $ifNull: ["$gamesPlayed", []] } }
+                    }
+                },
+                { $sample: { size: limit } },
+                { $skip: skips },
+                { $limit: limit }
+            ];
+            users = await USER.aggregate(pipeline);
+        }
+
+        const count = await USER.countDocuments({ _id: { $nin: [...friends, currentUser._id] } });
+
         res.status(200).json({
             users,
             total: count,
@@ -16,6 +77,6 @@ async function getAllUser(req, res, next) {
     } catch (error) {
         next(error)
     }
-} 
+}
 
 export default getAllUser
--- server/routes/user.js
+++ server/routes/user.js
@@ -6,31 +6,16 @@ import deleteUser from "../controllers/user/deleteUser.js"
 import getAllUser from "../controllers/user/getAllUser.js"
 import connectUser from "../controllers/user/connectUser.js"
 import disconnectUser from "../controllers/user/disconnectUser.js"
+
 const router = express.Router()
 
-// get a user
-const getAUserRoute = process.env.NODE_ENV === 'production' ? process.env.GET_USER_ROUTE : "/a/:userId"
-router.get(getAUserRoute, verifyJWT, getAUser)
+const getRoute = (productionRoute, developmentRoute) => process.env.NODE_ENV === 'production' ? process.env[productionRoute] : developmentRoute
 
-// update a user
-const updateUserRoute = process.env.NODE_ENV === 'production' ? process.env.UPDATE_USER_ROUTE : "/"
-router.put(updateUserRoute, verifyJWT, updateUser)
-
-// delete a user
-const deleteUserRoute = process.env.NODE_ENV === 'production' ? process.env.DELETE_USER_ROUTE : "/"
-router.delete(deleteUserRoute, verifyJWT, deleteUser)
-
-// get all users
-const getAllUserRoute = process.env.NODE_ENV === 'production' ? process.env.GET_ALL_USER_ROUTE : "/users"
-router.get(getAllUserRoute, getAllUser)
-
-// connect to a user
-const connectUserRoute = process.env.NODE_ENV === 'production' ? process.env.CONNECT_USER_ROUTE : "/:id/connect"
-router.post(connectUserRoute, verifyJWT, connectUser)
-
-// connect to a user
-const disconnectUserRoute = process.env.NODE_ENV === 'production' ? process.env.DISCONNECT_USER_ROUTE : "/:id/disconnect"
-router.delete(disconnectUserRoute, verifyJWT, disconnectUser)
-
+router.get(getRoute('GET_USER_ROUTE', '/a/:userId'), verifyJWT, getAUser)
+router.put(getRoute('UPDATE_USER_ROUTE', '/'), verifyJWT, updateUser)
+router.delete(getRoute('DELETE_USER_ROUTE', '/'), verifyJWT, deleteUser)
+router.get(getRoute('GET_ALL_USER_ROUTE', '/users'),verifyJWT, getAllUser)
+router.post(getRoute('CONNECT_USER_ROUTE', '/:id/connect'), verifyJWT, connectUser)
+router.delete(getRoute('DISCONNECT_USER_ROUTE', '/:id/disconnect'), verifyJWT, disconnectUser)
 
 export default router
