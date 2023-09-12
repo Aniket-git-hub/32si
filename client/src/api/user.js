8d3801a84c2bcde0ee04df1444168020f4f2cfcd
aniket-git-hub <singhdharmvir81@gmail.com>
2023-09-12 11:21:17 +0530

    fixed issue of production use route

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -38,7 +38,7 @@ const getEndpoint = (productionRoute, developmentRoute) => import.meta.env.VITE_
 
 export const updateUser = async (data) => handleRequest(getEndpoint('VITE_UPDATE_USER_ROUTE', '/user/'), data, "PUT")
 export const getAllUsers = async (data) => handleRequest(getEndpoint('VITE_ALL_USERS_ROUTE', '/user/users'), data, "GET")
-export const getAUser = async (username) => handleRequest(`${getEndpoint("VITE_GET_A_USER_ROUTE,", '/user/a/')}${username}`, null, "GET")
-export const connectUser = async (userId) => handleRequest(`${getEndpoint("VITE_CONNECT_USER_ROUTE,", '/user/')}${userId}/connect`, null, "POST")
-export const disconnectUser = async (userId) => handleRequest(`${getEndpoint("VITE_DISCONNECT_USER_ROUTE,", '/user/')}${userId}/disconnect`, null, "DELETE")
-export const acceptConnection = async (userId) => handleRequest(`${getEndpoint("VITE_ACCEPT_CONNECTION_ROUTE,", '/user/')}${userId}/accept`, null, "POST")
+export const getAUser = async (username) => handleRequest(`${getEndpoint("VITE_GET_A_USER_ROUTE", '/user/a/')}${username}`, null, "GET")
+export const connectUser = async (userId) => handleRequest(`${getEndpoint("VITE_CONNECT_USER_ROUTE", '/user/')}${userId}/connect`, null, "POST")
+export const disconnectUser = async (userId) => handleRequest(`${getEndpoint("VITE_DISCONNECT_USER_ROUTE", '/user/')}${userId}/disconnect`, null, "DELETE")
+export const acceptConnection = async (userId) => handleRequest(`${getEndpoint("VITE_ACCEPT_CONNECTION_ROUTE", '/user/')}${userId}/accept`, null, "POST")
