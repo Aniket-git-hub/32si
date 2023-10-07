731115b2a3a57e0028fdaeff9494111deef9b844
Aniket <singhdharmvir81@gmail.com>
2023-10-07 15:57:36 +0530

    organized imports

--- client/src/components/dashboard/RightSidePanel.jsx
+++ client/src/components/dashboard/RightSidePanel.jsx
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Avatar, Button, Box, HStack, Heading, Input, InputGroup, InputLeftElement, List, ListItem, Stack, VStack, Center, Text, AvatarBadge } from "@chakra-ui/react"
-import { FiSearch } from "react-icons/fi"
-import { useAuth } from "../../hooks/useAuth"
-import { useNavigate } from "react-router-dom"
-import { useAllData } from "../../hooks/useAllData"
-import { devPrint } from "../../utils/Helper"
-import { getProfilePicture, getSmallProfilePicture, searchUsers } from '../../api/user';
-import useDebounce from '../../hooks/useDebounce'
-import AvatarWithPreview from '../utils/AvatarWithPreview'
-import CustomModal from "../utils/CustomModal"
+import { AvatarBadge, Box, Center, HStack, Heading, Input, InputGroup, InputLeftElement, List, ListItem, Stack, Text } from "@chakra-ui/react";
+import React from 'react';
+import { FiSearch } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { getProfilePicture, getSmallProfilePicture } from '../../api/user';
+import { useAllData } from "../../hooks/useAllData";
+import { useAuth } from "../../hooks/useAuth";
+import AvatarWithPreview from '../utils/AvatarWithPreview';
+import CustomModal from "../utils/CustomModal";
 import FullTextSearchUsers from '../utils/FullTextSearchUsers';
 
 function RightSidePanel() {
@@ -48,8 +45,8 @@ function RightSidePanel() {
                 <AvatarWithPreview
                   size={"sm"}
                   name={friend?.name}
-                  smallURL={getSmallProfilePicture(friend?.profilePicture)}
-                  largeURL={getProfilePicture(friend?.profilePicture)}
+                  smallURL={getSmallProfilePicture(friend?.profilePhoto)}
+                  largeURL={getProfilePicture(friend?.profilePhoto)}
                 >
                   {onlineFriends?.includes(friend._id) && (
                     <AvatarBadge boxSize='1.25em' bg='green.500' title={`${friend.username} is online`} ></AvatarBadge>
