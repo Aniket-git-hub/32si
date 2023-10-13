fffec1c7817bc779c70dc229ba98ca096054b6aa
Aniket <singhdharmvir81@gmail.com>
2023-10-13 19:02:46 +0530

    ADDED:  permisson to join in

--- server/src/socketIOEventHandlers/gameEventHandler.ts
+++ server/src/socketIOEventHandlers/gameEventHandler.ts
@@ -8,23 +8,31 @@ interface User {
 interface Socket extends IOSocket {
   userId?: string;
 }
+interface Lobby {
+  users: string[];
+  count: number;
+  creator: string;
+}
 
-const rooms = new Map<string, number>();
+// Create a new Map to store the online lobbies
+const lobbies = new Map<string, { users: string[], count: number, creator: string }>();
 
 export const gameEventHandler = (socket: Socket, users: Map<string, User>) => {
   const io: Server = getIO();
 
   socket.on('createGame', ({ userId, gameLobbyId }) => {
-    const user = users.get(userId)
+    const user = users.get(userId);
     if (user) {
-      const room = rooms.get(gameLobbyId)
-      if (room && room >= 2) {
+      const lobby = lobbies.get(gameLobbyId) || { users: [], count: 0, creator: userId };
+      if (lobby.count >= 2) {
         console.log('Room is full');
         return;
       }
       socket.join(gameLobbyId);
-      console.log(`${userId} join the lobby ${gameLobbyId}`)
-      rooms.set(gameLobbyId, (rooms.get(gameLobbyId) || 0) + 1);
+      console.log(`${userId} join the lobby ${gameLobbyId}`);
+      (lobby as Lobby).users.push(userId);
+      lobby.count++;
+      lobbies.set(gameLobbyId, lobby);
       io.to(user.socketId).emit('gameCreated', { gameLobbyId });
     }
   });
@@ -32,14 +40,31 @@ export const gameEventHandler = (socket: Socket, users: Map<string, User>) => {
   socket.on('joinGame', ({ userId, gameLobbyId }) => {
     const user = users.get(userId);
     if (user) {
-      const room = rooms.get(gameLobbyId);
-      if (room && room > 2) {
+      const lobby = lobbies.get(gameLobbyId);
+      if (!lobby) {
+        console.log('Lobby does not exist');
+        return;
+      }
+      if (lobby.count >= 2) {
         console.log('Room is full');
         return;
       }
-      socket.join(gameLobbyId)
-      console.log(`${userId} join the lobby ${gameLobbyId}`)
-      rooms.set(gameLobbyId, (rooms.get(gameLobbyId) || 0) + 1);
+
+      // Emit an event to the creator asking for permission
+      io.to(lobby.creator).emit('requestJoin', { userId, gameLobbyId });
+    }
+  });
+
+  // When the creator gives permission, add the user to the lobby
+  socket.on('allowJoin', ({ userId, gameLobbyId }) => {
+    const user = users.get(userId);
+    const lobby = lobbies.get(gameLobbyId);
+    if (user && lobby && lobby.creator === socket.userId) {
+      socket.join(gameLobbyId);
+      console.log(`${userId} join the lobby ${gameLobbyId}`);
+      lobby.users.push(userId);
+      lobby.count++;
+      lobbies.set(gameLobbyId, lobby);
       io.to(user.socketId).emit('gameJoined', { gameLobbyId });
       socket.to(gameLobbyId).emit('userJoined', { userId });
     }
@@ -50,9 +75,15 @@ export const gameEventHandler = (socket: Socket, users: Map<string, User>) => {
     if (user) {
       socket.leave(gameLobbyId);
       console.log(`${userId} left the lobby ${gameLobbyId}`);
-      rooms.set(gameLobbyId, (rooms.get(gameLobbyId) || 0) - 1);
-      io.to(user.socketId).emit('gameLeft', { gameLobbyId });
-      socket.to(gameLobbyId).emit('userLeft', { userId });
+      const lobby = lobbies.get(gameLobbyId) || { users: [], count: 0, creator: '' };
+      const index = lobby.users.indexOf(userId);
+      if (index !== -1) {
+        lobby.users.splice(index, 1);
+        lobby.count--;
+        lobbies.set(gameLobbyId, lobby);
+        io.to(user.socketId).emit('gameLeft', { gameLobbyId });
+        socket.to(gameLobbyId).emit('userLeft', { userId });
+      }
     }
   });
 
